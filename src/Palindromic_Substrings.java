34bda63ae083c98a750b241afa81a12379874046
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-10 18:55:18 -0400

    Works but slow. Will try another implementation again

--- src/Palindromic_Substrings.java
+++ src/Palindromic_Substrings.java
@@ -4,39 +4,91 @@ public class Palindromic_Substrings {
 
 	public static void main(String[] args) {
 
-		String s = "abc";
-		
+		String s = "aaa";
+
 //		System.out.println(isPalindrome(s));
 		System.out.println(countSubstrings(s));
-		
+
 	}// main
 
-
 	public static int countSubstrings(String s) {
+
+		/*
+		 * Palindrome if: 1: Single Character 2: Two Characters - both same 3: If length
+		 * > 2, then s[first] === s[last] and the substring between the first and last
+		 * character is a palindrome
+		 */
+
+		boolean[][] used = new boolean[s.length()][s.length()];
+
+		for (int i = 0; i < used.length; i++) {
+
+			for (int j = i; j < used[i].length; j++) {
+
+				if (used[i][j] == true) {
+					continue;
+				} // if
+
+				if (isPalindrome(s.substring(i, j + 1))) {
+//					System.out.println(s.substring(i, j + 1));
+					used[i][j] = true;
+				} // if
+
+			} // for
+
+		} // for i
+
 		
-		return 0;
+//		System.out.println("*******************");
+		int count = 0;
+
+		for (int i = 0; i < used.length; i++) {
+
+			for (int j = i; j < used[i].length; j++) {
+
+				if (used[i][j] == true) {
+//					System.out.println(s.substring(i, j+1));
+					count++;
+				}
+
+			} // for j
+
+		} // for i
+
+		return count;
 	}// countSubstrings
 
-	
-	
 	public static boolean isPalindrome(String s) {
 
-		if (s.length() <= 1) return true;
-		
-		int end = s.length() - 1;
+		if (s.length() <= 1)
+			return true;
 
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == s.charAt(end)) {
-				end--;
+		if (s.length() == 2) {
+			if (s.charAt(0) == s.charAt(1)) {
+				return true;
 			} // if
 			else {
 				return false;
 			} // else
-		} // for
+		} // if
+
+		if (s.length() > 2) {
+
+			int end = s.length() - 1;
+
+			for (int i = 0; i < s.length(); i++) {
+				if (s.charAt(i) == s.charAt(end)) {
+					end--;
+				} // if
+				else {
+					return false;
+				} // else
+			} // for
+
+		} // if
 
 		return true;
 
 	}// isPalindrome
-	
-	
+
 }// palindromic substring
