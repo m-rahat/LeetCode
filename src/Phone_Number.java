7ff5e4861948814bea9f1e0b6a3beb3d3d5c7a4d
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-09 23:52:50 -0400

    Figured out a good chunk of regex

--- src/Phone_Number.java
+++ src/Phone_Number.java
@@ -1,20 +1,34 @@
-import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import java.util.*;
 
 public class Phone_Number {
 
 	public static void main(String [] args) {
 		
-		String input = "(929)-398-4910";
+		String input = "3472389619";
+
+		//the grouping was done using () in the regex
+		//so, ([0-9]{3}) is $1 aka group 1 and vice versa. this took me too long to figure out
+		String output = input.replaceFirst("([0-9]{3})([0-9]{3})([0-9]{4})", "($1)-$2-$3");
 		
-		System.out.println(validNumber(input));
+
+		System.out.println(output);
+		System.out.println(validNumber(output));
+		
 		
 	}//main
 	
 	public static boolean validNumber(String s) {
 		
 //		boolean result = Pattern.matches("[(][1-9]{3}[)][-][0-9]{3}[-][0-9]{4}", s);
-		boolean result = s.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}");
+//		boolean result = s.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}");
+		
+		Pattern pattern = Pattern.compile("[(][1-9]{3}[)][-][0-9]{3}[-][0-9]{4}");
+		Matcher matcher = pattern.matcher(s);
+		
+		boolean result = matcher.matches();
 		
 		return result;
 	}//boolean
