df3224d080bf344fc3d16429258e95da8d782103
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-13 18:15:07 -0400

    Triangle problem

--- src/Triangle.java
+++ src/Triangle.java
@@ -3,51 +3,65 @@ import java.util.stream.Collectors;
 
 public class Triangle {
 
-	public static void main(String [] args) {
+	public static void main(String[] args) {
+
+		List<List<Integer>> triangle = new ArrayList<>();
 		
-	}//main
-	
-    public int minimumTotal(List<List<Integer>> triangle) {
-        
-    	/*
-    	 * i'm given a list of lists.
-    	 * one first list.get(0), i'm assuming that the size would be 1. 
-    	 * then the list.size = 2, al the way upto the end of the list
-    	 * where list.get(n) = the end of the list.
-    	 * 
-    	 * first list size will always be zero, so total = first list item
-    	 * return the minimum possible value, from adding total to each number
-    	 * but with limited range of index - 1, index, and index + 1 range
-    	 * 
-    	 * [[-1],[2,3],[1,-1,-3]]
-    	 * out : -2
-    	 * expected: -1
-    	 */
-    	
-    	int index = 0;
-    	int total = triangle.get(0).get(0);
-    	
-    	for (int i = 1; i < triangle.size(); i++) {
-    		
-    		List<Integer> list = triangle.get(i);
-    		
-    		int result = total + list.get(index - 1);
-    		
-    		int range = index + 1;
-    		
-    		for (int j = index - 1; j < range; j++) {
-    			if (result + list.get(i) < result) {
-    				result = result + list.get(i);
-    				index = j;
-    			}//if
-    		}//for
-    		
-    		total += result;
-    		
-    	}//for
-    	
-    	return total;
-    }//minimumTotal
-	
-	
-}//Triangle
+		List<Integer> list = new ArrayList<>();
+		list.add(2);
+		
+	}// main
+
+	public static int minimumTotal(List<List<Integer>> triangle) {
+
+		/*
+		 * [[-1],[2,3],[1,-1,-3]] out : -2 expected: -1
+		 */
+
+		int total = triangle.get(0).get(0);
+		int index = 0;
+
+		for (int i = 1; i < triangle.size(); i++) {
+
+			List<Integer> list = triangle.get(i);
+
+			int index1 = index - 1;
+			int index2 = index;
+			int index3 = index + 1;
+
+			int result1 = Integer.MAX_VALUE;
+			int result2 = Integer.MAX_VALUE;
+			int result3 = Integer.MAX_VALUE;
+
+			if (list.get(index1) != null) {
+				result1 = total += list.get(index1);
+			} // if
+
+			if (list.get(index2) != null) {
+				result2 = total += list.get(index2);
+			} // if
+
+			if (list.get(index3) != null) {
+				result3 = total += list.get(index3);
+			}
+
+			if (result1 > result2 && result1 > result3) {
+				total = result1;
+				index = index1;
+			} // if
+			if (result2 > result1 && result2 > result3) {
+				total = result2;
+				index = index2;
+			} // if
+			if (result3 > result1 && result3 > result2) {
+				total = result3;
+				index = index3;
+			} // if
+
+		} // for i
+
+		return total;
+	}// minimumTotal
+
+
+}// Triangle
