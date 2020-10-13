a0f2c64b679882aadced142153c1c869cb6de683
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-13 02:46:02 -0400

    Inheritance Practice

---
+++ src/Inheritance_test.java
@@ -0,0 +1,27 @@
+
+public class Inheritance_test {
+
+	public static void main(String[] args) {
+
+		
+		A a = new A();
+	}
+
+}// Inheritance_Test
+
+class A {
+
+	
+	{
+		System.out.println("A created");
+	}
+	
+}// classA
+
+class B {
+
+	{
+		System.out.println("B created");
+	}
+	
+}// class B
