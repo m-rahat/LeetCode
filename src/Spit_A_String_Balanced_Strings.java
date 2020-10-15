4b98cb0b0525277a7fb7c4149fdec369f9553da8
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-15 16:45:25 -0400

    works

--- src/Spit_A_String_Balanced_Strings.java
+++ src/Spit_A_String_Balanced_Strings.java
@@ -10,41 +10,27 @@ public class Spit_A_String_Balanced_Strings {
 	}// main
 
 	public static int balancedStringSplit(String s) {
-
-		/*
-		 * iterate through the string if balanced, put the balanced string in a hashset
-		 * if not balanced, check the next one
-		 *
-		 * iterate through the string check substring, from i to substring j put result
-		 * of isbalanced into dp[][] as dp[i][j] = true means that substring [i][j] is
-		 * balanced
-		 *
-		 * iterater through the string see if substring i,j is in dp array if not,
-		 * calculate if substring(i, j) is balanced if balanced, then, put the result in
-		 * dp[]i[j] = true and then, put the substring in a hashset
-		 *
-		 * in the end, return the size of the hashset
-		 */
-		boolean[][] dp = new boolean[s.length()][s.length()];
-		Set<String> set = new HashSet<>();
 		
-		for (int i = 0; i < s.length(); i++) {
-			for (int j = i + 1; j < s.length(); j++) {
-				if (dp[i][j] == true) {
-					set.add(s.substring(i, j+1));
-				}//if
-				else {
-					boolean result = isBalanced(s.substring(i, j+1));
-					if (result == true) {
-						dp[i][j] = true;
-						set.add(s.substring(i, j+1));
-					}//if
-				}//else
-			}//for j
-		}//for i
+		if (s == null || s.length() == 0) {
+			return 0;
+		}//if
 		
-		System.out.println(set);
-		return set.size();
+		int left = 0, right = 0, score = 0;
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == 'L') {
+				left++;
+			}//if
+			else {
+				right++;
+			}//else
+			if (left == right) {
+				score++;
+			}//if
+			
+		}//for
+		return score;
+		
 	}// balancedStringSplit
 
 	public static boolean isBalanced(String st) {
