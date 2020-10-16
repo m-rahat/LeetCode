8e6fb8baebe7ba9b1e261abc88a0f8c3ed60dbcc
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-16 17:58:22 -0400

    work in progress
    still developing

--- src/Middle_Of_Linked_List.java
+++ src/Middle_Of_Linked_List.java
@@ -6,10 +6,30 @@ public class Middle_Of_Linked_List {
 
 	}// main
 
+	/*
+	 * I might have to see how many nodes there are in total. so i'll probably need to 
+	 * iterate through all the nodes, to see the size of the linked list.
+	 * once i have determined the size of the list, i have to find out if it is even or odd
+	 * if it is even, then i have to itearate to (n/2)+1 and give the result
+	 * 7/2 = 3 , so 4 is the ans
+	 * 9/2 = 4, so 5 is the ans
+	 * 
+	 * [1,2, 3, 4, 5]
+	 * output 4
+	 */
 	public ListNode middleNode(ListNode head) {
-
+	
+		int size = 1;
+		
+		while(head.next != null) {
+			size++;
+			head = head.next;
+		}//while
+		
+		System.out.println(size);
+		
+		return head.next;
 	}// middleNode
 
 }// MiddleOFLinkedList
-
 
