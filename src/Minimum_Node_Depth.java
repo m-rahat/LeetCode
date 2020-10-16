c67c79b545aeb449c0a47e9a56610073649ade5f
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-16 15:02:20 -0400

    works :)

--- src/Minimum_Node_Depth.java
+++ src/Minimum_Node_Depth.java
@@ -6,6 +6,18 @@ public class Minimum_Node_Depth {
 		
 	}//main
 	
+	/*
+	 * [2,null,3,null,4,null,5,null,6]
+	 * output: 1
+	 * expected: 5
+	 * 
+	 *				2 
+	 * 			null	3
+	 * 				null	4
+	 * 					null	5
+	 * 						null	6
+	 *				 				
+	 */
 	
     public static int minDepth(TreeNode root) {
         
@@ -13,6 +25,10 @@ public class Minimum_Node_Depth {
     	
     	int depthLeft = minDepth(root.left);
     	int depthRight = minDepth(root.right);
+    	
+    	if (depthLeft == 0 || depthRight == 0) {
+    		return 1 + depthLeft + depthRight;
+    	}
     	
     	return 1 + Math.min(depthLeft, depthRight);
     	
