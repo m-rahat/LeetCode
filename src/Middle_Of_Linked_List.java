b9ea67d9b80914403011734f05183a959182c062
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-16 17:47:05 -0400

    Class Creation

---
+++ src/Middle_Of_Linked_List.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Middle_Of_Linked_List {
+
+	public static void main(String[] args) {
+
+	}// main
+
+	public ListNode middleNode(ListNode head) {
+
+	}// middleNode
+
+}// MiddleOFLinkedList
+
+
