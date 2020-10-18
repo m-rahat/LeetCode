f47bfc14636d9a35d0d7a2943fe743658e890089
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-18 19:30:11 -0400

    kinda works

--- src/Lowest_Common_Ancestor_Binary_Tree.java
+++ src/Lowest_Common_Ancestor_Binary_Tree.java
@@ -2,30 +2,34 @@ import java.util.*;
 
 public class Lowest_Common_Ancestor_Binary_Tree {
 	
-	/*
-	 * p would say, oh look here are my ancestors in this list. i'm an ancestor of myself
-	 * q would say, oh look here are my ancestors too in this list. i'm an ancestor of myself
-	 * 
-	 * p&q: let's compare out treesets from the back to the front.
-	 * p&q: the first match will be our answer
-	 * 
-	 * p: 3 5 
-	 * q: 3 1
-	 * 
-	 *
-	 * 
-	 * p: 3 5
-	 * q: 3 5 2 4
-	 * 
-	 * 
-	 * 
-	 */
+	static TreeNode ans = new TreeNode();
 
 	public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
 		
-		
+		recurseTree(root, p, q);
+		return ans;
 	}// lowestCommonAncestor
+	
+	public static boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
+		
+		if (currentNode == null) return false;
+		
+		boolean left = recurseTree(currentNode.left, p, q);
+		boolean right = recurseTree(currentNode.right, p, q);
+		
+		boolean mid = false;
+		
+		if (currentNode == p || currentNode == q) {
+			mid = true;
+		}
+		
+		if (left == true || right == true) {
+			ans = currentNode;
+			return true;
+		}
+		
+		return false;
+	}//recurseTree
 
 }// classname
 
