9c447b00e61ff8cc5d5b755892c29f878c7b29de
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-18 19:32:23 -0400

    leetcode solution

--- src/Lowest_Common_Ancestor_Binary_Tree.java
+++ src/Lowest_Common_Ancestor_Binary_Tree.java
@@ -1,34 +1,41 @@
 import java.util.*;
 
 public class Lowest_Common_Ancestor_Binary_Tree {
-	
-	static TreeNode ans = new TreeNode();
+	private TreeNode ans;
 
-	public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		
-		recurseTree(root, p, q);
-		return ans;
-	}// lowestCommonAncestor
-	
-	public static boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
-		
-		if (currentNode == null) return false;
-		
-		boolean left = recurseTree(currentNode.left, p, q);
-		boolean right = recurseTree(currentNode.right, p, q);
-		
-		boolean mid = false;
-		
-		if (currentNode == p || currentNode == q) {
-			mid = true;
-		}
-		
-		if (left == true || right == true) {
-			ans = currentNode;
-		}
-		
-		return (left || right || mid);
-	}//recurseTree
+	public Lowest_Common_Ancestor_Binary_Tree() {
+	        // Variable to store LCA node.
+	        this.ans = null;
+	    }
 
+	private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
+
+		// If reached the end of a branch, return false.
+		if (currentNode == null) {
+			return false;
+		}
+
+		// Left Recursion. If left recursion returns true, set left = 1 else 0
+		int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
+
+		// Right Recursion
+		int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
+
+		// If the current node is one of p or q
+		int mid = (currentNode == p || currentNode == q) ? 1 : 0;
+
+		// If any two of the flags left, right or mid become True
+		if (mid + left + right >= 2) {
+			this.ans = currentNode;
+		}
+
+		// Return true if any one of the three bool values is True.
+		return (mid + left + right > 0);
+	}
+
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		// Traverse the tree
+		this.recurseTree(root, p, q);
+		return this.ans;
+	}
 }// classname
-
