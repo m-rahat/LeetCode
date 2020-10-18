11329c553473c3717ed39d7f21b4517fc4f6ed48
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-18 17:01:34 -0400

    it works yay

--- src/Partition_Array_Into_Three_Parts_With_Equal_Sum.java
+++ src/Partition_Array_Into_Three_Parts_With_Equal_Sum.java
@@ -4,8 +4,9 @@ public class Partition_Array_Into_Three_Parts_With_Equal_Sum {
 
 	public static void main(String [] args) {
 		
+//		int[] A = new int[] {18,12,-18,18,-19,-1,10,10};
+
 		int[] A = new int[] {3,3,6,5,-2,2,5,1,-9,4};
-		
 		System.out.println(canThreePartsEqualSum(A));
 		
 	}//main
@@ -15,38 +16,37 @@ public class Partition_Array_Into_Three_Parts_With_Equal_Sum {
         
     	if (A.length < 3) return false;
     	
-    	int[] sums = new int[A.length];
+    	int[] sums = new int[3];
     	int total = 0;
     	
     	for (int i = 0; i < A.length; i++) {
     		total += A[i];
     	}//for
     	
+    	int index = 0;
     	int k = 0;
-    	for (int i = 0; i < A.length; i++) {
-    		
-    		System.out.println(Arrays.toString(sums));
-    		
-    		if (sums[k] == total/3) k++;
-    		
-    		if (sums[k] + A[i] > total/3) {
-//    			System.out.println(sums[k] + " here");
-    			k++;
-    			sums[k] += A[i];
+    	for (int i = 0; i < A.length - 2; i++) {
+    		sums[0] += A[i];
+    		if (sums[0] == total/3) {
+    			index = i + 1;
+    			break;
     		}//if
-    		else {
-    			sums[k] += A[i];
-//    			System.out.println(sums[k] + " there");
-    		}//else
+    	}//for
+    	
+    	for (int i = index; i < A.length - 1; i++) {
+    		sums[1] += A[i];
+    		if (sums[1] == total/3) {
+    			index = i + 1;
+    			break;
+    		}//if
+    	}//for
+    	
+    	for (int i = index; i < A.length; i++) {
+    		sums[2] += A[i];
     		
     	}//for
-    	
-    	for (int i = 3; i < sums.length; i++) {
-    		sums[2] += sums[i];
-    	}//for
-    	
+    		
     	System.out.println(Arrays.toString(sums));
-    
     	if (sums[0] == sums[1] && sums[0] == sums[2]) {
     		return true;
     	}//if
@@ -56,16 +56,3 @@ public class Partition_Array_Into_Three_Parts_With_Equal_Sum {
 	
 }//class name
 
-//add up to see what the max number could be
-//tthen on each iteration, find out how many
-//iterations it takes to get upto that max number / 3
-//
-//if max/3 found, start adding upto another number. if num >/ max/3, then return false
-//
-//if second num found, start adding 3rd number
-//
-//if num1 == num2 == num3 false,
-//
-//what do we do?
-//		
-		
