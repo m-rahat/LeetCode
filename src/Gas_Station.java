91cb3cff6fc384f1aac64961d6a5fe8426e9debe
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-20 19:49:35 -0400

    Brute force works

--- src/Gas_Station.java
+++ src/Gas_Station.java
@@ -4,44 +4,48 @@ public class Gas_Station {
 
 	public static void main(String[] args) {
 
-		int[]  gas = new int[] { 1, 2, 3, 4, 5 };
+		int[] gas = new int[] { 1, 2, 3, 4, 5 };
 		int[] cost = new int[] { 3, 4, 5, 1, 2 };
 
 //		int start = 2;
 //		for(int i = 0; i <= gas.length; i++) {
 //			System.out.println(gas[(start + i + gas.length) % gas.length]);
 //		}
-		
+
 		System.out.println(canCompleteCircuit(gas, cost));
 
 	}// main
 
-
 	public static int canCompleteCircuit(int[] gas, int[] cost) {
 
-		int tank = 0;
-		boolean madeIt = false;
+		int totalGas = 0;
+		int totalCost = 0;
 
 		for (int i = 0; i < gas.length; i++) {
-			
-			int startStation = i;
-			
-			for (int j = 0; j <= gas.length; j++) {
-				int startIndex = (startStation + j + gas.length) % gas.length;
-				if (tank + gas[startIndex] >= cost[startIndex]) {
-					tank = tank + gas[startIndex];
-					System.out.println(tank);
-				}
-				else {
-					break;
-				}
-			}//for
-			
-			if (madeIt) return startStation;
-			
+			totalGas += gas[i];
+			totalCost += cost[i];
 		} // for
-		
-		System.out.println(tank);
+
+		if (totalCost > totalGas)
+			return -1;
+
+		for (int i = 0; i < gas.length; i++) {
+
+			int tank = 0;
+			boolean possible = true;
+
+			for (int j = 0; j < gas.length; j++) {
+				int station = (j + i + gas.length) % gas.length;
+				tank = tank + gas[station] - cost[station];
+				if (tank < 0) {
+					possible = false;
+					break;
+				} // if
+			} // for j
+			if (possible)
+				return i;
+
+		} // for i
 
 		return -1;
 	}// canCompleteCircuirt
