7f04336d8ace747b2bdd6bb06b9a77b1fd1344b1
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-10-22 16:30:28 -0400

    Gas staion

--- src/Gas_Station.java
+++ src/Gas_Station.java
@@ -5,17 +5,17 @@ public class Gas_Station {
 	public static void main(String[] args) {
 
 		int[] gas = new int[] { 1, 2, 3, 4, 5 };
-		int[] cost = new int[] { 3, 4, 5, 1, 2 };
-
-//		int start = 2;
-//		for(int i = 0; i <= gas.length; i++) {
-//			System.out.println(gas[(start + i + gas.length) % gas.length]);
-//		}
-
+		int[] cost = new int[]{ 3, 4, 5, 1, 2 };
+		//gas					-2 -2 -2 3  3
+		//sum 					
+		
+		
+		
 		System.out.println(canCompleteCircuit(gas, cost));
 
 	}// main
 
+	//brute force solution
 	public static int canCompleteCircuit(int[] gas, int[] cost) {
 
 		int totalGas = 0;
@@ -49,5 +49,27 @@ public class Gas_Station {
 
 		return -1;
 	}// canCompleteCircuirt
+	
+	//Leetcode solution
+    public int canCompleteCircuitV2(int[] gas, int[] cost) {
+        int sum = 0;
+        for(int i=0; i<gas.length; i++) {
+            gas[i] -= cost[i];
+            sum += gas[i];
+        }
+        if(sum < 0)
+            return -1;
+        sum = 0;
+        int index = 0;
+        for(int i=0; i<gas.length; i++) {
+            sum += gas[i];
+            if(sum < 0) {
+                sum = 0;
+                index = i+1;
+            }
+        }
+        
+        return index;
+    }
 
 }// Gas_Station
