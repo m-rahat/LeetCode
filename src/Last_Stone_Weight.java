30fd976c890af024cc0a725b3e2f7e67ce14c1e4
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-12-01 15:29:36 -0500

    works

--- src/Last_Stone_Weight.java
+++ src/Last_Stone_Weight.java
@@ -1,14 +1,40 @@
 import java.util.*;
 public class Last_Stone_Weight {
 
-    public int lastStoneWeight(int[] stones) {
+    public static int lastStoneWeight(int[] stones) {
      
+    	List<Integer> list = new ArrayList<>();
+    	for (int i = 0; i < stones.length; i++) {
+    		list.add(stones[i]);
+    	}//for
     	
+    	do {
+    		Collections.sort(list);
+    		int n1 = list.get(list.size() - 1);
+    		int n2 = list.get(list.size() - 2);
+    		if (n1 == n2) {
+    			list.remove(list.size()-1);
+    			list.remove(list.size()-2);
+    		}
+    		else {
+    			int n = Math.abs(n1 - n2);
+    			list.remove(list.size()-1);
+    			list.remove(list.size()-1);
+    			list.add(n);
+    		}
+    	}
+    	while(list.size() > 2);
+    	
+    	System.out.println(list + " result");
     	
     	return 0;
     }//lastStoneWeight
 	
 	public static void main(String[] args) {
+		
+		int[] stones = {2, 7, 4, 1, 8, 1};
+		
+		System.out.println(lastStoneWeight(stones));
 		
 	}//main
 }//Class
