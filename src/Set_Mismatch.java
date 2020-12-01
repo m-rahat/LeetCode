f52e73767295cb9148126f529c946e0997bb2895
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-12-01 14:44:48 -0500

    work in progress

--- src/Set_Mismatch.java
+++ src/Set_Mismatch.java
@@ -4,16 +4,34 @@ public class Set_Mismatch {
 
 	public static int[] findErrorNums(int[] nums) {
 
+		Arrays.sort(nums);
+		System.out.println(Arrays.toString(nums));
+		int[] returnArray = new int[2];
 		Set<Integer> set = new HashSet<>();
-		for(int i = 0; i < nums.length; i++) {
-			set.add(nums[i]);
+		for (int i = 0; i < nums.length; i++) {
+			if (set.contains(nums[i])) {
+				System.out.println("here");
+				returnArray[0] = nums[i];
+				break;
+			}//if
+			else {
+				set.add(nums[i]);
+			}//else
 		}//for
 		
+		for (int i = 0; i < nums.length; i++) {
+			if (i+1 != nums[i]) {
+				returnArray[1] = i+1;
+			}//if
+		}//for
+		
+		System.out.println(Arrays.toString(returnArray));
+		return returnArray;
 	}// findErrorNums
 
 	public static void main(String[] args) {
 
-		int[] nums = { 1, 2, 2, 4 };
+		int[] nums = { 1,5,3, 2, 2, 7, 6, 4, 8, 9 };
 
 		System.out.println(findErrorNums(nums));
 	}// main
