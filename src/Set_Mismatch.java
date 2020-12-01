f20f5add5c7a8d9c7ea28929825b466701a4b603
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-12-01 14:51:30 -0500

    faster than 10% of all

--- src/Set_Mismatch.java
+++ src/Set_Mismatch.java
@@ -4,28 +4,34 @@ public class Set_Mismatch {
 
 	public static int[] findErrorNums(int[] nums) {
 
-		Arrays.sort(nums);
-		System.out.println(Arrays.toString(nums));
 		int[] returnArray = new int[2];
-		Set<Integer> set = new HashSet<>();
+		
+		Set<Integer> set1 = new HashSet<>();
+		for (int i = 1; i < nums.length+1; i++) {
+			set1.add(i);
+		}//for
+		
+		Set<Integer> set2 = new HashSet<>();
 		for (int i = 0; i < nums.length; i++) {
-			if (set.contains(nums[i])) {
-				System.out.println("here");
+			if (set2.contains(nums[i])) {
 				returnArray[0] = nums[i];
-				break;
 			}//if
 			else {
-				set.add(nums[i]);
+				set2.add(nums[i]);
 			}//else
 		}//for
 		
-		for (int i = 0; i < nums.length; i++) {
-			if (i+1 != nums[i]) {
-				returnArray[1] = i+1;
+		Iterator it = set1.iterator();
+		while(it.hasNext()) {
+			int n = (int) it.next();
+			if (!set2.contains(n)) {
+				returnArray[1] = n;
+				break;
 			}//if
-		}//for
+		}//while
 		
 		System.out.println(Arrays.toString(returnArray));
+		
 		return returnArray;
 	}// findErrorNums
 
