b831d57348b3cde51279ae8e093203dc7a5ce7f3
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-12-05 14:26:25 -0500

    String permutation problem

---
+++ src/Match_String.java
@@ -0,0 +1,20 @@
+import java.util.*;
+/*
+ * Given an input string, make the computer match the string in the shortest possible time
+ */
+public class Match_String {
+
+	
+	public static void main(String [] args) {
+		
+		String input = "The quick brown fox jumps over the lazy dog";
+	
+		String output = "";
+		
+		while(input.hashCode() != output.hashCode()) {
+			
+		}//while
+		
+	}//main
+	
+}//class
