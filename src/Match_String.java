e2a01803e9159b9cd257b679ab5aa343c4aa1f4e
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-12-06 14:21:49 -0500

    optimization in progress

--- src/Match_String.java
+++ src/Match_String.java
@@ -4,16 +4,50 @@ import java.util.*;
  */
 public class Match_String {
 
+    private static void permute(String str, int l, int r, Set<String> set) 
+    { 
+        if (l == r) 
+//            System.out.println(str); 
+        	set.add(str);
+        else { 
+            for (int i = l; i <= r; i++) { 
+                str = swap(str, l, i); 
+                permute(str, l + 1, r, set); 
+                str = swap(str, l, i); 
+            } 
+        } 
+    } //permute
+  
+    /** 
+     * Swap Characters at position 
+     * @param a string value 
+     * @param i position 1 
+     * @param j position 2 
+     * @return swapped string 
+     */
+    public static String swap(String a, int i, int j) 
+    { 
+        char temp; 
+        char[] charArray = a.toCharArray(); 
+        temp = charArray[i]; 
+        charArray[i] = charArray[j]; 
+        charArray[j] = temp; 
+        return String.valueOf(charArray); 
+    }//swap 
+
 	
 	public static void main(String [] args) {
 		
-		String input = "The quick brown fox jumps over the lazy dog";
+		long start = System.currentTimeMillis();
+		String input = "the quick ";
+
+		System.out.println("Input length : " +input.length());
+		Set<String> set = new HashSet<>();
+		permute(input, 0, input.length()-1, set);
+		System.out.println("Set size : " + set.size());
+		long end = System.currentTimeMillis();
+		System.out.println("Runtime : " +(end-start)/1000);
 	
-		String output = "";
-		
-		while(input.hashCode() != output.hashCode()) {
-			
-		}//while
 		
 	}//main
 	
