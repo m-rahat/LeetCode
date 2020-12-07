10df428caf103f63d5dd3a73a6ffcc60f37de67e
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-12-07 01:04:48 -0500

    work in progress

--- src/Match_String.java
+++ src/Match_String.java
@@ -1,60 +1,58 @@
 import java.util.*;
+
 /*
  * Given an input string, make the computer match the string in the shortest possible time
  */
-public class Match_String extends Thread{
+public class Match_String extends Thread {
 
-    private static void permute(String str, int l, int r, Set<String> set, int factorial) 
-    { 
-        if (l == r) {
+	private static void permute(String str, int l, int r, Set<String> set, long factorial) {
+		if (l == r) {
 //            System.out.println(str); 
-        	if (set.size() == factorial) return;
-        	set.add(str);
-        }
-        else { 
-            for (int i = l; i <= r; i++) { 
-                str = swap(str, l, i); 
-                permute(str, l + 1, r, set, factorial); 
-                str = swap(str, l, i); 
-            } 
-        } 
-    } //permute
-  
-    public static String swap(String a, int i, int j) 
-    { 
-        char temp; 
-        char[] charArray = a.toCharArray(); 
-        temp = charArray[i]; 
-        charArray[i] = charArray[j]; 
-        charArray[j] = temp; 
-        return String.valueOf(charArray); 
-    }//swap 
+			set.add(str);
+		} // if
+		else {
+			for (int i = l; i <= r; i++) {
+				str = swap(str, l, i);
+				permute(str, l + 1, r, set, factorial);
+				str = swap(str, l, i);
+			} // for
+		} // else
+	} // permute
 
-    public static int factorial(int n) {
-    	
-    }//factorial
-    
-    public void run() {
-    	
-    }//run
-	
-	public static void main(String [] args) {
-		
+	public static String swap(String a, int i, int j) {
+		char temp;
+		char[] charArray = a.toCharArray();
+		temp = charArray[i];
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return String.valueOf(charArray);
+	}// swap
+
+	public static long factorial(int n) {
+		long ans = 1;
+		for (int i = 0; i < n; i++) {
+			ans *= i;
+		}
+		return ans;
+	}// factorial
+
+	public void run() {
+
+	}// run
+
+	public static void main(String[] args) {
+
 		long start = System.currentTimeMillis();
-		
-		String input = "thequickbr";
-		System.out.println("Input length : " +input.length());
+
+		String input = "thequickbro";
+		System.out.println("Input length : " + input.length());
 		Set<String> set = new HashSet<>();
-		
-		int factorial = 
-		
-		permute(input, 0, input.length()-1, set, 3628800);
+		long fact = factorial(input.length());
+		permute(input, 0, input.length() - 1, set, fact);
 		System.out.println("Set size : " + set.size());
 		long end = System.currentTimeMillis();
-		System.out.println("Runtime : " +(end-start)/1000);
-	
-		
-		
-	}//main
-	
-}//class
+		System.out.println("Runtime : " + (end - start) / 1000);
+
+	}// main
+
+}// class
