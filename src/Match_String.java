d990a4caa41911882338c603cb63d49795d99e46
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-12-07 00:52:40 -0500

    work in progress

--- src/Match_String.java
+++ src/Match_String.java
@@ -2,29 +2,24 @@ import java.util.*;
 /*
  * Given an input string, make the computer match the string in the shortest possible time
  */
-public class Match_String {
+public class Match_String extends Thread{
 
-    private static void permute(String str, int l, int r, Set<String> set) 
+    private static void permute(String str, int l, int r, Set<String> set, int factorial) 
     { 
-        if (l == r) 
+        if (l == r) {
 //            System.out.println(str); 
+        	if (set.size() == factorial) return;
         	set.add(str);
+        }
         else { 
             for (int i = l; i <= r; i++) { 
                 str = swap(str, l, i); 
-                permute(str, l + 1, r, set); 
+                permute(str, l + 1, r, set, factorial); 
                 str = swap(str, l, i); 
             } 
         } 
     } //permute
   
-    /** 
-     * Swap Characters at position 
-     * @param a string value 
-     * @param i position 1 
-     * @param j position 2 
-     * @return swapped string 
-     */
     public static String swap(String a, int i, int j) 
     { 
         char temp; 
@@ -35,19 +30,30 @@ public class Match_String {
         return String.valueOf(charArray); 
     }//swap 
 
+    public static int factorial(int n) {
+    	
+    }//factorial
+    
+    public void run() {
+    	
+    }//run
 	
 	public static void main(String [] args) {
 		
 		long start = System.currentTimeMillis();
-		String input = "the quick ";
-
+		
+		String input = "thequickbr";
 		System.out.println("Input length : " +input.length());
 		Set<String> set = new HashSet<>();
-		permute(input, 0, input.length()-1, set);
+		
+		int factorial = 
+		
+		permute(input, 0, input.length()-1, set, 3628800);
 		System.out.println("Set size : " + set.size());
 		long end = System.currentTimeMillis();
 		System.out.println("Runtime : " +(end-start)/1000);
 	
+		
 		
 	}//main
 	
